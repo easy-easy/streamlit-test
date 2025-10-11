0d781bdb6283e883543ab99f92b9bd73d299d85d
easy-easy <minazukitombo@gmail.com>
2025-10-11 12:23:45 +0900

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("はじめてのStreamlitアプリ")
+st.write("Hello, World!")
+
+# def main():
+#     st.write("Hello from streamlit-test!")
+
+
+# if __name__ == "__main__":
+#     main()
